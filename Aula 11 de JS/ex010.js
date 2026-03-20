3d7ba7d2572afcd68b5617b5b649cfe4dbb2c111
gaugusto815-ui <g.augusto815@gmail.com>
2026-03-20 07:21:13 -0300

    ex de JS

---
+++ Aula 11 de JS/ex010.js
@@ -0,0 +1,7 @@
+var vel = 30.5
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
