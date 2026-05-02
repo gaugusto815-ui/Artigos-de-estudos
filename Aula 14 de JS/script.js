6c51218e7d03a281e8d520f22b21082b7fd9d831
gaugusto815-ui <g.augusto815@gmail.com>
2026-05-02 14:23:27 -0300

    meio da aula

--- Aula 14 de JS/script.js
+++ Aula 14 de JS/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 20
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora <12) {
+    // bom dia!
+    img.src = 'dia.png'
+    document.body.style.background = '#958166'
+} else if (hora >= 12 && hora < 18){
+    // boa tarde!
+    img.src = 'tarde.png'
+    document.body.style.background = '#C44D00'
+} else {
+    // boa noite!
+    img.src = 'noite.png'
+    document.body.style.background = '#0A1727'
+}
+}
